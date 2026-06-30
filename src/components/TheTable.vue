ef161797a6de261aa3a659584528fa7632e6749e
Стекольщиков Александр <alexstekk@yandex.ru>
2026-06-30 18:30:26 +0300

    AGGrid WIP

--- src/components/TheTable.vue
+++ src/components/TheTable.vue
@@ -1,45 +1,72 @@
-<script lang="ts" setup>
-import { AllCommunityModule, ModuleRegistry, type ColDef } from 'ag-grid-community';
-import { AgGridVue } from 'ag-grid-vue3';
-import { ref } from 'vue';
+<script setup lang="ts">
+import { AgGridVue } from "ag-grid-vue3"; // Vue Grid Logic
 
-// Register all Community features
-ModuleRegistry.registerModules([AllCommunityModule]);
+import { treeStoreItems } from "@/tree-store/data";
+import { TreeStore } from "@/tree-store/tree-store";
+import type { Item } from "@/tree-store/types";
+import {
+    AllEnterpriseModule,
+    LicenseManager,
+    ModuleRegistry,
+    RowGroupingModule,
+    TreeDataModule,
+    type AutoGroupColumnDef,
+    type ColDef,
+    type GridReadyEvent
+} from "ag-grid-enterprise";
+import { ref } from "vue";
+ModuleRegistry.registerModules([AllEnterpriseModule, RowGroupingModule, TreeDataModule,]);
+LicenseManager.setLicenseKey("<your license key>");
 
 
-// Row Data Interface
-interface IRow {
-    make: string;
-    model: string;
-    price: number;
-    electric: boolean;
-}
 
-const rowData = ref<IRow[]>([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
-    { make: "Fiat", model: "500", price: 15774, electric: false },
-    { make: "Nissan", model: "Juke", price: 20675, electric: false },
-]);
+const mapDataToAGGrid = (items: Item[]) => {
+    const store = new TreeStore(items);
+    const buildNode = (item: Item) => {
+        const children = store.getChildren(item.id);
+        if (children.length) {
+            item.children = children.map(item => buildNode(item));
+        }
+        return item;
+    };
 
-const colDefs = ref<ColDef<IRow>[]>([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" },
-]);
-
-const defaultColDef = {
-    flex: 1,
+    const roots = items.filter(item => item.parent === null);
+    return roots.map(root => buildNode(root));
 };
+
+const mappedData = mapDataToAGGrid(treeStoreItems);
+
+console.log("mappedData", mappedData);
+
+const columnDefs = ref<ColDef[]>([
+
+    { field: "label", headerName: "Наименование" },
+]);
+
+const defaultColDef = ref<ColDef>({
+    flex: 1,
+});
+
+const autoGroupColumnDef = ref<AutoGroupColumnDef>({
+    headerName: "Категория",
+    valueGetter: (params) => (params.data.children?.length ? "Группа" : "Элемент"),
+    cellRendererParams: {
+        suppressCount: true,
+    },
+
+});
+
+const rowData = ref<any[] | null>(mappedData);
+const treeDataChildrenField = ref("children");
+const groupDefaultExpanded = ref(-1);
+const onGridReady = (params: GridReadyEvent) => {
+    gridApi.value = params.api;
+};
+
 </script>
 
 <template>
-    <ag-grid-vue style="flex: 1 1 auto; height: 600px" :columnDefs="colDefs" :rowData="rowData"
-        :defaultColDef="defaultColDef">
-    </ag-grid-vue>
+    <ag-grid-vue style="width: 100%; height: 50rem;" @grid-ready="onGridReady" :columnDefs="columnDefs"
+        :defaultColDef="defaultColDef" :autoGroupColumnDef="autoGroupColumnDef" :rowData="rowData" :treeData="true"
+        :treeDataChildrenField="treeDataChildrenField" :groupDefaultExpanded="groupDefaultExpanded"></ag-grid-vue>
 </template>
-
-<style scoped></style>
