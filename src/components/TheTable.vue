8d40cacc639eac0e8b61d3e006e876320fec90c6
Стекольщиков Александр <alexstekk@yandex.ru>
2026-06-30 20:50:17 +0300

    Соответствие макету.

--- src/components/TheTable.vue
+++ src/components/TheTable.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import { AgGridVue } from "ag-grid-vue3"; // Vue Grid Logic
-
 import type { Item } from "@/tree-store/types";
 import {
-    AllEnterpriseModule,
     ModuleRegistry,
-    RowGroupingModule,
+    themeQuartz,
     TreeDataModule,
     type AutoGroupColumnDef,
     type ColDef
 } from "ag-grid-enterprise";
+import { AgGridVue } from "ag-grid-vue3";
 import { ref } from "vue";
-ModuleRegistry.registerModules([AllEnterpriseModule, RowGroupingModule, TreeDataModule,]);
+ModuleRegistry.registerModules([TreeDataModule]);
 
+// Мб избыточно
 export interface MappedItem extends Item {
     children?: Item[] | null;
 }
@@ -22,6 +21,17 @@ export interface Props {
 }
 
 const { data } = defineProps<Props>();
+const borderColor = 'oklch(0 0 0 / 0.15)';
+const border = { style: 'solid', width: 1, color: borderColor };
+
+const myTheme = themeQuartz.withParams({
+    wrapperBorder: false,
+    headerRowBorder: border,
+    headerColumnBorder: border,
+    borderColor: borderColor,
+    rowBorder: border,
+
+});
 
 const columnDefs = ref<ColDef[]>([
     {
@@ -29,8 +39,16 @@ const columnDefs = ref<ColDef[]>([
         valueGetter: "node.rowIndex + 1",
         pinned: 'left',
         width: 100,
+        cellClass: "text-bold",
+        cellStyle: { borderRight: "none" }
     },
-    { field: "label", headerName: "Наименование" },
+    {
+        field: "label",
+        headerName: "Наименование",
+        cellClassRules: {
+            "text-bold": (params) => params.data.children?.length,
+        },
+    },
 ]);
 
 const defaultColDef = ref<ColDef>({
@@ -43,7 +61,7 @@ const autoGroupColumnDef = ref<AutoGroupColumnDef>({
     valueGetter: (params) => (params.data.children?.length ? "Группа" : "Элемент"),
     cellClassRules: {
         "text-bold": (params) => params.data.children?.length,
-    }
+    },
 });
 
 const rowData = ref<Item[] | null>(data);
@@ -54,7 +72,7 @@ const groupDefaultExpanded = ref(-1);
 <template>
     <h2 class="text-center">MStroy | AG Grid | <a href="https://t.me/alexstekk">Александр Стекольщиков</a></h2>
     <ag-grid-vue style="width: 100%; height: 50rem;" :columnDefs="columnDefs" :defaultColDef="defaultColDef"
-        :autoGroupColumnDef="autoGroupColumnDef" :rowData="rowData" :treeData="true"
+        :autoGroupColumnDef="autoGroupColumnDef" :rowData="rowData" :treeData="true" :theme="myTheme"
         :treeDataChildrenField="treeDataChildrenField" :groupDefaultExpanded="groupDefaultExpanded"></ag-grid-vue>
 </template>
 
